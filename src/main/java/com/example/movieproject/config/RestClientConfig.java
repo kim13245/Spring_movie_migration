24c3a460b57bf4d09beec68b4a28b2e3acaf75d3
KimSeHyeon <61528451+kim13245@users.noreply.github.com>
2026-09-15 18:38:03 +0900

    refactor: TMDB 연동 클라이언트를 RestTemplate에서 RestClient로 전환

--- src/main/java/com/example/movieproject/config/RestClientConfig.java
+++ src/main/java/com/example/movieproject/config/RestClientConfig.java
@@ -3,16 +3,19 @@ package com.example.movieproject.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 @Configuration
-public class RestTemplateConfig {
+public class RestClientConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
+    public RestClient restClient() {
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
         factory.setConnectTimeout(5000); // 5 seconds
         factory.setReadTimeout(5000); // 5 seconds
-        return new RestTemplate(factory);
+
+        return RestClient.builder()
+                .requestFactory(factory)
+                .build();
     }
 }
--- src/main/java/com/example/movieproject/external/tmdb/TmdbClient.java
+++ src/main/java/com/example/movieproject/external/tmdb/TmdbClient.java
@@ -6,14 +6,16 @@ import com.example.movieproject.external.tmdb.dto.TmdbSearchResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @Component
 @RequiredArgsConstructor
 public class TmdbClient {
 
-    private final RestTemplate restTemplate;
+    private final RestClient restClient;
 
     @Value("${tmdb.api-key}")
     private String apiKey;
@@ -24,30 +26,42 @@ public class TmdbClient {
     private static final String LANGUAGE = "ko-KR";
 
     public TmdbMovieDetailResponse getMovieDetails(Integer movieId) {
-        String url = UriComponentsBuilder.fromUriString(baseUrl + "/movie/" + movieId)
+        URI uri = UriComponentsBuilder.fromUriString(baseUrl + "/movie/" + movieId)
                 .queryParam("api_key", apiKey)
                 .queryParam("language", LANGUAGE)
-                .toUriString();
+                .build()
+                .toUri();
 
-        return restTemplate.getForObject(url, TmdbMovieDetailResponse.class);
+        return restClient.get()
+                .uri(uri)
+                .retrieve()
+                .body(TmdbMovieDetailResponse.class);
     }
 
     public TmdbCreditsResponse getCredits(Integer movieId) {
-        String url = UriComponentsBuilder.fromUriString(baseUrl + "/movie/" + movieId + "/credits")
+        URI uri = UriComponentsBuilder.fromUriString(baseUrl + "/movie/" + movieId + "/credits")
                 .queryParam("api_key", apiKey)
                 .queryParam("language", LANGUAGE)
-                .toUriString();
+                .build()
+                .toUri();
 
-        return restTemplate.getForObject(url, TmdbCreditsResponse.class);
+        return restClient.get()
+                .uri(uri)
+                .retrieve()
+                .body(TmdbCreditsResponse.class);
     }
 
     public TmdbSearchResponse searchMovie(String title) {
-        String url = UriComponentsBuilder.fromUriString(baseUrl + "/search/movie")
+        URI uri = UriComponentsBuilder.fromUriString(baseUrl + "/search/movie")
                 .queryParam("api_key", apiKey)
                 .queryParam("query", title)
                 .queryParam("language", LANGUAGE)
-                .toUriString();
+                .build()
+                .toUri();
 
-        return restTemplate.getForObject(url, TmdbSearchResponse.class);
+        return restClient.get()
+                .uri(uri)
+                .retrieve()
+                .body(TmdbSearchResponse.class);
     }
 }
